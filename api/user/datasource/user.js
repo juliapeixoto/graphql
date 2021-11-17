79d1fec7a1c7f6a4fdd88f968ea3272adb1116b1
juliap <juliap@ciandt.com>
2021-11-17 15:25:38 -0300

    first commit

---
+++ api/user/datasource/user.js
@@ -0,0 +1,51 @@
+const { RESTDataSource } = require("apollo-datasource-rest");
+
+class UsersAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = "http://localhost:3000";
+  }
+
+  async getUsers() {
+    const users = await this.get("/users");
+    return users.map(async (user) => ({
+      id: user.id,
+      name: user.name,
+      online: user.online,
+      role: await this.get(`/roles/${user.role}`),
+    }));
+  }
+
+  async getUserById(id) {
+    const user = await this.get(`/users/${id}`);
+    user.role = await this.get(`/roles/${user.role}`);
+    return user;
+  }
+
+  async createUser(user) {
+    const users = await this.get("/users");
+    user.id = users.length + 1;
+    const role = await this.get(`roles?type=${user.role}`);
+    await this.post("users", { ...user, role: role[0].id });
+    return {
+      ...user,
+      role: role[0],
+    };
+  }
+
+  async updateUser(newData) {
+    const role = await this.get(`roles?type=${newData.role}`);
+    await this.put(`users/${newData.id}`, { ...newData, role: role[0].id });
+    return {
+      ...newData,
+      role: role[0],
+    };
+  }
+
+  async deleteUser(id) {
+    await this.delete(`users/${id}`);
+    return id;
+  }
+}
+
+module.exports = UsersAPI;
